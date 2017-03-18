4d2832bab945077104fdcb632e4f1816c419115c
SolominVA <SolominVA@gmail.com>
2017-03-18 23:56:05 +0600

    complete

--- sketchs/Harp/Harp.ino
+++ sketchs/Harp/Harp.ino
@@ -1,61 +1,77 @@
-//#include <ardumidi.h>
-
 int notes [24];
 int prevNote [24];
 int notePins[] = {22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 23, 25, 27, 29, 31, 33, 35, 37, 39, 41, 43, 45}; 
+int instruments[] = {46, 0, 24, 40, 56, 65, 73, 33, 14, 19, 30};
 int BUTTON_PIN = 18;
 int COMAND_ON = 0x90;
 int COMAND_OFF = 0x80;
-unsigned long lastRead;
-int instrument;
-int analog11;
-int note_on = 0;
+int lastInstrument = 0;
+volatile int instrNumber = 0;
+volatile unsigned long lastRead;
+boolean playFirst;
+unsigned long lastPlayTime = 0;
 
 void setup() {
   pinMode(46, OUTPUT);
-  instrument = 0;
+  instrNumber = 0;
   lastRead = 0;
   
   for (int i = 0; i < 24; i++) {
     pinMode(notePins[i], INPUT);
   }
   for (int i = 0; i < 24; i++) {
-    prevNote[i] = 1;
+    prevNote[i] = 0;
   }
   
   analogWrite(46, 50);
   Serial.begin(38400);
-  readAllInputs();
+  attachInterrupt(5, onButtonPressed, CHANGE); 
+  playFirst = true;
 }
 
 void loop() {
   
-  if ((millis() - lastRead) > 200)  {
-    analog11 = analogRead(A11);
-    if (analog11 > 500) {
-      instrument = instrument + 1;
-      //Serial.write(0xC0);
-      //Serial.write(instrument & 0x7F);
-      //Serial.println(chanelNumber);
-    }
-    if (instrument > 64) {instrument = 0;}
-    lastRead = millis();
-    
+  for (int i = 0; i < 24; i++) {
+    notes[i] = 0;
   }
- 
-  readAllInputs();
   
+  for (int i = 0; i < 100; i++) {
+    for (int j = 0; j < 24; j++) {
+      if (digitalRead(notePins[j])) {notes[j] = 1;}
+    }
+  }
+  
+  if (lastInstrument != instrNumber) {
+  for (int i = 0; i < 24; i++) {
+    if (prevNote[i] = 0) {
+      playNote(i, 0, 127, 1);
+      prevNote[i] = 1;
+    }
+  }
+    Serial.write(0xC0);
+    Serial.write(instruments[instrNumber] & 0x7F);
+    lastInstrument = instrNumber;
+  }
+
   for (int i = 0; i < 24; i++) {
     if (prevNote[i] != notes[i]) {
       playNote(i, 0, 127, notes[i]);
       prevNote[i] = notes[i];
     }
-  
-  /*playNote(0, 0, 120, note_on);
-  if (note_on) {note_on = 0;}
-  else {note_on = 1;}
-  delay(1000);*/
+  }
+
+  if ((millis() - lastPlayTime) > 60000) {
+    Serial.write(0xC0);
+    Serial.write(instruments[0] & 0x7F); 
+  }
 }
+
+void onButtonPressed() {
+  if (((millis() - lastRead) > 200) && digitalRead(BUTTON_PIN))  {
+    instrNumber = instrNumber + 1;
+    if (instrNumber > 10) {instrNumber = 0;}
+  }
+  lastRead = millis();
 }
 
 void playNote(int note, int channel, int velocity, int noteOff) {
@@ -63,33 +79,13 @@ void playNote(int note, int channel, int velocity, int noteOff) {
   if (noteOff) {cmd = COMAND_OFF;}
   else {cmd = COMAND_ON;}
   cmd = cmd  | (channel & 0x0F);
-  
-  //midi_note_on(chanel, 
-  //Serial.write(0xA0+chanel);
-  //Serial.write(60 + convertNote(note));
-  //Serial.write(120);
-  
+  if (playFirst) {
+    Serial.write(0xC0);
+    Serial.write(instruments[0] & 0x7F);  
+    playFirst = false;
+  }
   Serial.write(cmd);
-  Serial.write(60 + convertNote(note));
+  Serial.write(48 + note);
   Serial.write(velocity);
-  //if (noteOff) {Serial.write(velocity);}
-  //else {Serial.write(0x00);}
-  //delay(500);
+  lastPlayTime = millis();
 }
-
-void readAllInputs() {
-  
-  for (int i = 0; i < 24; i++) {
-  notes[i] = 1;
-  }
-  for (int i = 0; i < 1000; i++) {
-    for (int j = 0; j < 24; j++) {
-      if (!digitalRead(notePins[j])) {notes[j] = 0;}
-    }
-  }
-}
-
-int convertNote(int note) {
-  return note;
-  }  
-
